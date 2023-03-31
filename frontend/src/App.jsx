2fc10b6738055a3ba59ce2ca3d7091b2359b3642
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-03-31 22:35:07 +0200

    completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import PhoneCard from "./components/PhoneCard";
+import PhoneDetails from "./components/PhoneDetails";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  const [phones, setPhones] = useState([]);
+
+  async function allPhones() {
+    try {
+      console.log("quelquechose");
+      const response = await axios.get("http://localhost:5005/phones");
+
+      console.log(response);
+      setPhones(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    allPhones();
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<PhoneCard phones={phones} />} />
+        <Route path="/:id" element={<PhoneDetails />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
